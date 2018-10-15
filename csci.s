2cea5b5a648695177aeb6c912e4d565bda37e9db
Dacia Copeland <dacia.copeland@bison.howard.edu>
2018-10-15 16:11:55 -0400

    second commit
    made a few changes with program description

--- csci.s
+++ csci.s
@@ -1,1 +1,9 @@
-#This program take id number and first name and last name and print it out.
+#This program takes id number, first name and last name, and prints it out.
+.data 
+
+#here will be the student id and the name 
+
+.main
+
+#here will be the program that reads the name and print it out.
+#have to use syscalls to print out the names.
