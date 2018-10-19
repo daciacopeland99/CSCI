ee758fa1e73e7b0727661a3b43cdd36c5bad9810
Dacia Copeland <dacia.copeland@bison.howard.edu>
2018-10-18 21:37:07 -0400

    Example Codes
    Play around with example lines, run them, and see what they do

--- csci.s
+++ csci.s
@@ -7,7 +7,14 @@ out_string: .asciiz "\n@02849198\n"
 #use as an example of output 
 
 .main
-
 #here will be the program that reads the name and print it out.
 #have to use syscalls to print out the names and exit code
+li $v0, 4 
+la $a0, out_string
+#run and see what this does
+
+syscall 
+#exit program
+
+
 
